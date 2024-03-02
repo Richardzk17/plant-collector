e9d964e3d6d70305c9f6199246b7932c65dbef34
richardzk17 <richardhernandezk17@gmail.com>
2024-03-01 19:01:06 -0500

    about route added

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def about(request):
+  return HttpResponse('<h1>About the Plant Collector</h1>')
