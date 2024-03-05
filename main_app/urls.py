876e413c624c6434e2523a2557d7bed65028996e
richardzk17 <richardhernandezk17@gmail.com>
2024-03-04 19:06:32 -0500

    added styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.Home.as_view(), name='home'),
-  path('about/', views.about, name='about'),
   path('plants/', views.plant_index, name='plant-index'),
   path('plants/<int:plant_id>/', views.plant_detail, name='plant-detail'),
   path('plants/create/', views.PlantCreate.as_view(), name='plant-create'),
